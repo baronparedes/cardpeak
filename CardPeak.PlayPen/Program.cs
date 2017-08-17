d009aadc5f36625435837950af15587056faf68f
Baron Paredes <baronpatrick.paredes@gmail.com>
2017-08-18 02:58:25 +0800

    base implementation repository

---
+++ CardPeak.PlayPen/Program.cs
@@ -0,0 +1,24 @@
+﻿using CardPeak.Repository.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardPeak.PlayPen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new CardPeakDbContext())
+            {
+                var repo = new UserRepository(context);
+                var user = repo.Get(0);
+
+                Console.WriteLine(user.Email);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ CardPeak.Repository.EF/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using CardPeak.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardPeak.Repository.EF
+{
+    public sealed class UserRepository : Repository<User>, IUserRepository
+    {
+        private readonly CardPeakDbContext DomainContext;
+        public UserRepository(CardPeakDbContext context) : base(context)
+        {
+            this.DomainContext = context;
+        }
+    }
+}
