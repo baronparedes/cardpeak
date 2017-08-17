eca26b1c834197c071cfed3dcb27dd40af843e8a
Baron Paredes <baronpatrick.paredes@gmail.com>
2017-08-17 23:33:29 +0800

    Added transactions.

---
+++ CardPeak.Database/dbo/Tables/ApprovalTransaction.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ApprovalTransaction]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [BankId] INT NOT NULL, 
+    [CardCategoryId] INT NOT NULL, 
+    [AgentId] INT NOT NULL, 
+    [Units] DECIMAL(19, 4) NOT NULL DEFAULT 1, 
+    [Amount] DECIMAL(19, 4) NOT NULL, 
+	[ProductType] VARCHAR(100) NOT NULL,
+    [ReferenceNumber1] VARCHAR(50) NULL, 
+    [ReferenceNumber2] VARCHAR(50) NULL, 
+    [Client] VARCHAR(255) NULL, 
+    [ApprovalDate] DATE NOT NULL, 
+    CONSTRAINT [FK_ApprovalTransaction_Agent] FOREIGN KEY ([AgentId]) REFERENCES [Agent]([AgentId])
+)
---
+++ CardPeak.Database/dbo/Tables/DebitCreditTransaction.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[DebitCreditTransaction]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [AgentId] INT NOT NULL, 
+    [Amount] DECIMAL(19, 4) NOT NULL, 
+    [Remarks] VARCHAR(255) NOT NULL, 
+    CONSTRAINT [FK_DebitCreditTransaction_Agent] FOREIGN KEY ([AgentId]) REFERENCES [Agent]([AgentId])
+)
