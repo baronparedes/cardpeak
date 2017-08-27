3348b167cb1dec57d60f9d07f1aacb5e045f4815
Baron Paredes <baronpatrick.paredes@gmail.com>
2017-08-27 23:41:18 +0800

    optimization

--- CardPeak.UI.ClientApp/src/components/views/transactions/ApprovalTransactionDetailRowLayout.tsx
+++ CardPeak.UI.ClientApp/src/components/views/transactions/ApprovalTransactionDetailRowLayout.tsx
@@ -13,26 +13,26 @@ const ApprovalTransactionDetailRowLayout = (props: ApprovalTransactionDetailRowL
             <Col mdHidden
                 lgHidden
                 smHidden
-                xsHidden={!this.props.isHeader}>
+                xsHidden={!props.isHeader}>
                 <span className="text-center spacer-left">Approval Transactions</span>
             </Col>
-            <Col md={2} lg={2} sm={2} xsHidden={this.props.isHeader}>
-                {this.props.isHeader ? "bank" : this.props.transaction.bank.description}
+            <Col md={2} lg={2} sm={2} xsHidden={props.isHeader}>
+                {props.isHeader ? "bank" : props.transaction.bank.description}
             </Col>
-            <Col md={2} lg={2} sm={2} xsHidden={this.props.isHeader}>
-                {this.props.isHeader ? "card category" : this.props.transaction.cardCategory.description}
+            <Col md={2} lg={2} sm={2} xsHidden={props.isHeader}>
+                {props.isHeader ? "card category" : props.transaction.cardCategory.description}
             </Col>
-            <Col md={2} lg={2} sm={2} xsHidden={this.props.isHeader}>
-                {this.props.isHeader ? "product" : this.props.transaction.productType}
+            <Col md={2} lg={2} sm={2} xsHidden={props.isHeader}>
+                {props.isHeader ? "product" : props.transaction.productType}
             </Col>
-            <Col md={3} lg={3} sm={2} xsHidden={this.props.isHeader}>
-                {this.props.isHeader ? "client" : this.props.transaction.client}
+            <Col md={3} lg={3} sm={2} xsHidden={props.isHeader}>
+                {props.isHeader ? "client" : props.transaction.client}
             </Col>
-            <Col md={1} lg={1} sm={2} xsHidden={this.props.isHeader}>
-                {this.props.isHeader ? "approval date" : dateFormat(this.props.transaction.approvalDate)}
+            <Col md={1} lg={1} sm={2} xsHidden={props.isHeader}>
+                {props.isHeader ? "approval date" : dateFormat(props.transaction.approvalDate)}
             </Col>
-            <Col md={2} lg={2} sm={2} xsHidden={this.props.isHeader}>
-                {this.props.isHeader ? "amount" : <span className="row-amount currency text-highlight">{this.props.transaction.amount}</span>}
+            <Col md={2} lg={2} sm={2} xsHidden={props.isHeader}>
+                {props.isHeader ? "amount" : <span className="row-amount currency text-highlight">{props.transaction.amount}</span>}
             </Col>
         </Row>
     )
