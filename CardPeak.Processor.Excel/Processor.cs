f50595faec0b2fadba5a8f04ab7da093dfa2b2bb
Baron Paredes <baronpatrick.paredes@gmail.com>
2017-09-01 04:51:37 +0800

    place holder for uploads

---
+++ CardPeak.Processor.Excel/Processor.cs
@@ -0,0 +1,14 @@
+﻿using CardPeak.Core.Processor;
+using System;
+using System.IO;
+
+namespace CardPeak.Processor.Excel
+{
+    public sealed class Processor : IProcessor
+    {
+        public void Process(FileInfo file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
