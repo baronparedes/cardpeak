1edaf278fb923283a0e1dfab51dc52a12146c3f8
Baron Paredes <baronpatrick.paredes@gmail.com>
2017-09-20 11:35:17 +0800

    versioning

--- CardPeak.Processor.Excel/Properties/AssemblyInfo.cs
+++ CardPeak.Processor.Excel/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6b88189e-0ce5-4712-86e3-dbab05f7267c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
