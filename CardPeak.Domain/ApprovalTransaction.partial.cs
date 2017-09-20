833498e94153c9cf720ba444abf7fc0b21f7d86c
Baron Paredes <baronpatrick.paredes@gmail.com>
2017-09-20 17:13:20 +0800

    validate pre-upload to check if the excel has duplicates

---
+++ CardPeak.Domain/ApprovalTransaction.partial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardPeak.Domain
+{
+	public partial class ApprovalTransaction
+	{
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+
+		public override bool Equals(object obj)
+		{
+			var compare = obj as ApprovalTransaction;
+			if (compare == null)
+			{
+				return base.Equals(obj);
+			}
+
+			return this.Id == compare.Id &&
+				this.AgentId == compare.AgentId &&
+				this.BankId == compare.BankId &&
+				this.CardCategoryId == compare.CardCategoryId &&
+				this.ProductType == compare.ProductType &&
+				this.ReferenceNumber1 == compare.ReferenceNumber1 &&
+				this.ReferenceNumber2 == compare.ReferenceNumber2 &&
+				this.Client == compare.Client &&
+				this.IsDeleted == compare.IsDeleted;
+		}
+	}
+}
