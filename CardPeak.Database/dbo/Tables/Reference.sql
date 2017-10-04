3203c4c89e5bed9be1c68865225d3ad3dac8c9d0
Baron Paredes <baronpatrick.paredes@gmail.com>
2017-10-04 16:35:32 +0800

    fixed issue when adding a new bank reference.

--- CardPeak.Database/dbo/Tables/Reference.sql
+++ CardPeak.Database/dbo/Tables/Reference.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[Reference]
 (
 	[ReferenceId] INT NOT NULL IDENTITY, 
-    [ReferenceTypeId] INT NOT NULL, 
-    [Description] VARCHAR(100) NOT NULL, 
-    CONSTRAINT [FK_Reference_ReferenceType] FOREIGN KEY ([ReferenceTypeId]) REFERENCES [ReferenceType]([ReferenceTypeId]), 
-    CONSTRAINT [PK_Reference] PRIMARY KEY ([ReferenceId])
+	[ReferenceTypeId] INT NOT NULL, 
+	[Description] VARCHAR(100) NOT NULL, 
+	CONSTRAINT [FK_Reference_ReferenceType] FOREIGN KEY ([ReferenceTypeId]) REFERENCES [ReferenceType]([ReferenceTypeId]), 
+	CONSTRAINT [PK_Reference] PRIMARY KEY ([ReferenceId]), 
+	CONSTRAINT [CK_Reference_Unique] UNIQUE(ReferenceTypeId, [Description])
 )
