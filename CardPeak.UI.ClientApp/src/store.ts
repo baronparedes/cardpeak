be765c99f5247ca299de1ffddc41cd639bbc73a0
Baron Paredes <baronpatrick.paredes@gmail.com>
2023-04-26 10:44:59 +0800

    fix(default_rates) - Fixed default rates not adding new items

--- CardPeak.UI.ClientApp/src/store.ts
+++ CardPeak.UI.ClientApp/src/store.ts
@@ -1,6 +1,7 @@
 ﻿import { applyMiddleware, createStore, Store } from 'redux';
 import thunk from 'redux-thunk';
 import reducers, { RootState } from './services/reducers';
+import { createLogger } from "redux-logger";
 
 let middleware;
 if (__NODE_ENV__ === 'production') {
